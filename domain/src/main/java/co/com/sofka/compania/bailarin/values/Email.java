eedf2997a3420d87433e943f0ccdf292383c6c1c
certhakzu <pealveco@yahoo.com>
2022-06-30 21:18:20 -0500

    Objetos de Valor de Bailarin, implementados

--- domain/src/main/java/co/com/sofka/compania/bailarin/values/Email.java
+++ domain/src/main/java/co/com/sofka/compania/bailarin/values/Email.java
@@ -2,16 +2,35 @@ package co.com.sofka.compania.bailarin.values;
 
 import co.com.sofka.domain.generic.ValueObject;
 
+import java.util.Objects;
+
 public class Email implements ValueObject {
 
-    private final String email;
+    private final String value;
 
-    public Email(String email) {
-        this.email = email;
+    public Email(String value) {
+
+        this.value = Objects.requireNonNull(value);
+        if (value.isBlank()){
+            throw new IllegalArgumentException("Email no puede venir en Blanco");
+        }
     }
 
     @Override
     public String value() {
-        return email;
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Email that = (Email) obj;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
--- domain/src/main/java/co/com/sofka/compania/bailarin/values/ExperienciaId.java
+++ domain/src/main/java/co/com/sofka/compania/bailarin/values/ExperienciaId.java
@@ -3,4 +3,16 @@ package co.com.sofka.compania.bailarin.values;
 import co.com.sofka.domain.generic.Identity;
 
 public class ExperienciaId extends Identity {
+
+    public ExperienciaId(){
+
+    }
+
+    private ExperienciaId(String id){
+        super(id);
+    }
+
+    public static ExperienciaId of(String id){
+        return new ExperienciaId(id);
+    }
 }
